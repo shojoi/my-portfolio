452119c9381c549223c22ca0d4d0a3375c9cbc22
shojoi <shobha.code@outlook.com>
2022-10-10 12:20:54 +1100

    git add projects

--- src/components/Post.js
+++ src/components/Post.js
@@ -6,7 +6,7 @@ function Post() {
   const [postData, setPost] = useState(null);
 
   useEffect(() => {
-    sanityClient.fetch(`*[_type]=="post"] {
+    sanityClient.fetch(`*[_type =="post"] {
       title,
       slug,
       mainImage {
@@ -34,16 +34,24 @@ function Post() {
           Welcome to my page of blog posts
         </h2>
         <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-8'>
-          <article>
-            <Link>
-              <span>
-                <img />
-                <span>
-                  <h3></h3>
+          {postData && postData.map((post, index) => (
+            <article>
+              <Link to={"/post/" + post.slug.current} key={index}>
+                <span className='block h-64 relative rounded shadow leading-snug bg-white border-l-8 border-green-400'>
+                  <img
+                    src={post.mainImage.asset.url}
+                    alt={post.mainImage.alt}
+                    className="w-full h-full rounded-r object-cover absolute"
+                  />
+                  <span className='block relative h-full flex justify-end items-end pr-4 pb-4'>
+                    <h3 className='text-gray-800 text-lg font-bold px-3 py-4 bg-red-700 text-red-100 bg-opacity-75'>
+                      {post.title}
+                    </h3>
+                  </span>
                 </span>
-              </span>
-            </Link>
-          </article>
+              </Link>
+            </article>
+          ))}
         </div>
       </section>
     </main>
